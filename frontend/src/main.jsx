2b56554c7883f7c5929b5891a073eef6b088366f
Jose Garcia <jc2k03@hotmail.com>
2024-06-14 23:20:32 -0400

    Creación de componentes Navbar y NavListDrawer, instalación e importación de react-router-dom y creación del diseño de temas principal de la aplicación web

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,16 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-import {CssBaseline} from "@mui/material";
+//librería para rutas
+import { BrowserRouter } from "react-router-dom";
 
+//librería para manejar Temas y estilos
+import {CssBaseline, ThemeProvider, createTheme} from "@mui/material";
+
+//fuentes roboto
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+//tema de la página
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#00bcd4',
+    },
+    secondary: {
+      main: '#006064',
+    },
+  },
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CssBaseline/>
-    <App />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <CssBaseline/>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 )
