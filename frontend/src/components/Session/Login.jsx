01158dd4139275671119972733e66575e5066d4e
Jose Garcia <jc2k03@hotmail.com>
2024-06-15 05:08:12 -0400

    Mejoras en login

--- frontend/src/components/Session/Login.jsx
+++ frontend/src/components/Session/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, TextField, Box } from '@mui/material';
+import { Button, TextField, Box, Typography, Link, autocompleteClasses, Container } from '@mui/material';
 import API from '../../Api';
 import { useNavigate } from 'react-router-dom';
 
@@ -21,29 +21,52 @@ const Login = () => {
     };
 
     return (
-        <Box component="form" onSubmit={handleSubmit}>
-        <TextField
-            label="Email"
-            type="email"
-            value={Email}
-            onChange={(e) => setEmail(e.target.value)}
-            fullWidth
-            margin="normal"
-            required
-        />
-        <TextField
-            label="Password"
-            type="password"
-            value={Password}
-            onChange={(e) => setPassword(e.target.value)}
-            fullWidth
-            margin="normal"
-            required
-        />
-        <Button type="submit" variant="contained" color="primary" fullWidth>
-            Login
-        </Button>
-        </Box>
+        <Container sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100vh',
+            }}
+            >
+            <Box component="form" onSubmit={handleSubmit} sx={{
+                border: 2,
+                borderColor: 'inherit',
+                padding: 3,
+                margin: 'auto',
+                width: '70%',
+                }}
+            >
+            <Typography variant='h5' align='center' color='primary'>Inicio de sesión</Typography>
+            <TextField
+                label="Email"
+                type="email"
+                value={Email}
+                onChange={(e) => setEmail(e.target.value)}
+                fullWidth
+                margin="normal"
+                required
+            />
+            <TextField
+                label="Password"
+                type="password"
+                value={Password}
+                onChange={(e) => setPassword(e.target.value)}
+                fullWidth
+                margin="normal"
+                required
+            />
+            <Button type="submit" variant="contained" color="primary" fullWidth>
+                Login
+            </Button>
+            <Typography variant="body2" align="center" mt={2}>
+                ¿No tienes una cuenta?{' '}
+                <Link component="button" onClick={() => navigate('/registro')} color="primary">
+                    Regístrate aquí
+                </Link>
+            </Typography>
+            </Box>
+        </Container>
     );
 };
 
